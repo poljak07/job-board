565d7d35314f5c7622cc27c9167ca2008eca9eed
poljak07 <acmeta4@gmail.com>
2024-07-17 22:11:53 +0200

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,10 @@
 <?php
 
-use App\Models\Job;
-use Illuminate\Support\Arr;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(5);
-    return view('jobs', [
-      'jobs' => $jobs
-    ]);
-});
+Route::resource('jobs', JobController::class);
 
-Route::get('jobs/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('job', ['job' => $job]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
